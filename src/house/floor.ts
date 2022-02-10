b4e820bd85ecf4317688adbfe137f6e1383d8343
maxmeng93 <maxmeng93@gmail.com>
2022-02-10 19:46:31 +0800

    update

--- src/house/floor.ts
+++ src/house/floor.ts
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { CSG } from 'three-csg-ts';
-import { initLight } from './utils';
+import skyPanoImg from './sky-dome-panorma.jpg';
 
 interface FloorConfig {
   // 长x
@@ -34,11 +34,7 @@ export default class Floor {
 
     const { floors, scene } = this.config;
 
-    // floors.forEach((f, i) => {
-    //   this.storey(f, i);
-    // });
-
-    const storeyMeshList = floors.map((f, i) => this.storey(f, i));
+    const storeyMeshList = floors.map((f, i) => this.storey(f, i, floorScene));
 
     storeyMeshList.forEach((s, i) => {
       floorScene.add(s);
@@ -57,8 +53,13 @@ export default class Floor {
     scene.add(floorScene);
   }
 
-  storey(storeyConfig: FloorConfig, index: number): THREE.Mesh {
+  storey(storeyConfig: FloorConfig, index: number, floorScene: THREE.Object3D): THREE.Mesh {
     const { width, height, depth } = storeyConfig;
+
+    var material = new THREE.MeshLambertMaterial({
+      color: 0xff1111,
+      wireframe: false,
+    });
 
     const geometry = new THREE.BoxGeometry(width, height, depth);
     const boxMesh = new THREE.Mesh(geometry);
@@ -73,9 +74,26 @@ export default class Floor {
     // 窗
     const windowgeo = new THREE.BoxGeometry(5, 5, 5);
     windowgeo.applyMatrix4(new THREE.Matrix4().makeTranslation(25, 0, 0));
-    const window = new THREE.Mesh(windowgeo);
+    let window = new THREE.Mesh(windowgeo);
     window.updateMatrix();
     emptyCube = CSG.subtract(emptyCube, window);
+
+    window = new THREE.Mesh(windowgeo, material.clone());
+
+    new THREE.TextureLoader().load(skyPanoImg, function (texture) {
+      const materialclone = material.clone();
+      materialclone.map = texture;
+      window.material = materialclone;
+
+      window.material.opacity = 0.4;
+      window.material.transparent = true;
+      texture.mapping = THREE.EquirectangularReflectionMapping;
+      window.position.y = 5;
+      window.scale.x = 0.5;
+    });
+
+    window.position.set(0, 0, 0);
+    floorScene.add(window);
 
     if (index === 0) {
       // 门
@@ -86,13 +104,12 @@ export default class Floor {
       emptyCube = CSG.subtract(emptyCube, door);
     }
 
-    var material = new THREE.MeshLambertMaterial({
+    emptyCube.material = new THREE.MeshLambertMaterial({
       color: 0xdadada,
       side: THREE.DoubleSide,
       // wireframe: true,
     });
 
-    emptyCube.material = material;
     return emptyCube;
   }
 }
