e162363f8aee2a95e74d0a7af67847fef7760ea0
maxmeng93 <maxmeng93@gmail.com>
2022-02-26 16:07:06 +0800

    模型加载封装

--- src/robot/index.ts
+++ src/robot/index.ts
@@ -1,8 +1,8 @@
 import * as THREE from 'three';
 import Stats from 'stats.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { initCamera, initRenderer, initLight, initAxes } from './utils';
+import { module1, module2, module3, module4, kago5 } from './models';
 
 export function init() {
   // 时间
@@ -32,73 +32,21 @@ export function init() {
   new OrbitControls(camera, renderer.domElement);
   container.appendChild(renderer.domElement);
 
+  let mixer: THREE.AnimationMixer | null = null;
   let robot: THREE.Object3D = new THREE.Object3D();
 
-  // // 加载头盔
-  // const loader1 = new GLTFLoader().setPath('/models/DamagedHelmet/glTF/')
-  // loader1.load('DamagedHelmet.gltf', function(gltf) {
-  //   console.log(gltf)
-  //   gltf.scene.scale.set(20, 20, 20);
-  //   gltf.scene.position.x -= 50;
-  //   scene.add(gltf.scene);
-  // })
+  // module1(scene);
+  // module2(scene);
+  // kago5(scene);
 
-  // // 加载机器人模型
-  // const loader2 = new GLTFLoader().setPath('/models/cute_little_robot/');
-  // loader2.load('scene.gltf', function (gltf) {
-  //     gltf.scene.scale.set(20, 20, 20);
-  //     gltf.scene.rotateY(-Math.PI * 0.8);
-  //     scene.add(gltf.scene);
-  //   }
-  // );
+  module3(scene).then((res: any) => {
+    robot = res.robot;
+    mixer = res.mixer;
+  });
 
-  let mixer: THREE.AnimationMixer | null = null;
-  // 加载直立机器人
-  const loader3 = new GLTFLoader().setPath('/models/biped_robot/')
-  loader3.load('scene.gltf', function(gltf) {
-    console.log('robot gltf', gltf)
-    robot = gltf.scene;
-    gltf.scene.scale.set(0.5, 0.5, 0.5);
-    gltf.scene.position.x += 50;
-    scene.add(gltf.scene);
-
-    // 添加骨架
-    const skeleton = new THREE.SkeletonHelper(gltf.scene);
-    skeleton.visible = true;
-    scene.add(skeleton);
-
-    mixer = new THREE.AnimationMixer(gltf.scene);
-    mixer.clipAction(gltf.animations[0]).play();
-  })
-
-  // // 加载粉色直立机器人（动画）
-  // const loader5 = new GLTFLoader().setPath('/models/');
-
-  // loader5.load('Xbot.glb', function (gltf) {
-  //   console.log('xbot', gltf);
-  //   gltf.scene.scale.set(40, 40, 40);
-  //   gltf.scene.rotateY(-Math.PI * 0.8);
-  //   scene.add(gltf.scene);
-
-  //   const skeleton = new THREE.SkeletonHelper(gltf.scene);
-  //   skeleton.visible = true;
-  //   scene.add(skeleton);
-
-  //   mixer = new THREE.AnimationMixer(gltf.scene);
-  //   const action = mixer.clipAction(gltf.animations[6]);
-  //   action.play();
-  // });
-
-  // 加载kago5
-  // const loader5 = new GLTFLoader().setPath('/models/kago5/');
-  // loader5.load('kago5.gltf', function (gltf) {
-  //   gltf.scene.scale.set(2, 2, 2);
-  //   gltf.scene.rotateY(-Math.PI * 0.8);
-  //   scene.add(gltf.scene);
-  // }, function(xhr) {
-  //   console.log('loaded', xhr.loaded)
-  // }, function(err) {
-  //   console.log('err', err);
+  // module4(scene).then((res: any) => {
+  //   robot = res.robot;
+  //   mixer = res.mixer;
   // });
 
   render();
@@ -111,7 +59,6 @@ export function init() {
     mouse.y = -((e.clientY / renderer.domElement.clientHeight) * 2) + 1;
 
     raycaster.setFromCamera(mouse, camera);
-    console.log('robot.children', robot.children);
     var intersects = raycaster.intersectObjects(robot.children);
     console.log('intersects', intersects);
 
---
+++ src/robot/models.ts
@@ -0,0 +1,105 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+
+/**
+ * 
+ * @param path 模型路径
+ * @param gltf 模型
+ * @param progress 加载进度回调
+ * @returns 
+ */
+function loadGltfModule({ path = '', gltf }: { path?: string, gltf: string }, progress?: Function) {
+  return new Promise((resolve, reject) => {
+    const loader = new GLTFLoader().setPath(path);
+    loader.load(gltf, function(module) {
+      console.log(`模型：${gltf}`, module);
+      resolve(module);
+    }, function(xhr) {
+      progress?.(xhr)
+    }, function(err) {
+      reject(err);
+    });
+  });
+}
+
+// 科技风头盔模型
+export function module1(scene: THREE.Scene) {
+  return loadGltfModule({ 
+    path: '/models/DamagedHelmet/glTF/', 
+    gltf: 'DamagedHelmet.gltf'
+  }).then((gltf: any) => {
+    gltf.scene.scale.set(20, 20, 20);
+    gltf.scene.position.x -= 50;
+    scene.add(gltf.scene);
+  });
+}
+
+// 履带机器人模型
+export function module2(scene: THREE.Scene) {
+  return loadGltfModule({ 
+    path: '/models/cute_little_robot/', 
+    gltf: 'scene.gltf'
+  }).then((gltf: any) => {
+    gltf.scene.scale.set(20, 20, 20);
+    gltf.scene.rotateY(-Math.PI * 0.8);
+    scene.add(gltf.scene);
+  });
+}
+
+// 大黄蜂模型
+export function module3(scene: THREE.Scene) {
+
+  return loadGltfModule({ path: '/models/biped_robot/', gltf: 'scene.gltf' }).then((gltf: any) => {
+    const robot = gltf.scene;
+    robot.scale.set(0.5, 0.5, 0.5);
+    robot.position.x += 50;
+    scene.add(robot);
+
+    // 添加骨架
+    const skeleton = new THREE.SkeletonHelper(robot);
+    skeleton.visible = true;
+    scene.add(skeleton);
+
+    const mixer = new THREE.AnimationMixer(robot);
+    mixer.clipAction(gltf.animations[0]).play();
+
+    return { robot, mixer };
+  })
+}
+
+
+// 加载粉色直立机器人（动画）
+export function module4(scene: THREE.Scene) {
+  return loadGltfModule({ 
+    path: '/models/', 
+    gltf: 'Xbot.glb'
+  }).then((gltf: any) => {
+    const robot = gltf.scene;
+
+    robot.scale.set(40, 40, 40);
+    robot.rotateY(-Math.PI * 0.8);
+    scene.add(robot);
+
+    const skeleton = new THREE.SkeletonHelper(robot);
+    skeleton.visible = true;
+    scene.add(skeleton);
+
+    const mixer = new THREE.AnimationMixer(robot);
+    const action = mixer.clipAction(gltf.animations[6]);
+    action.play();
+
+    return { robot, mixer };
+  });
+}
+
+// kago5
+export function kago5(scene: THREE.Scene) {
+  return loadGltfModule({ 
+    path: '/models/kago5/', 
+    gltf: 'kago5.gltf'
+  }).then((gltf: any) => {
+    gltf.scene.scale.set(2, 2, 2);
+    gltf.scene.rotateY(-Math.PI * 0.8);
+    scene.add(gltf.scene);
+  });
+}
